052eae4a8d443169134ecc165fb4254ee9e14412
gogulpradeep <mgogulpradeep@gmail.com>
2025-09-25 18:03:13 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -19,5 +19,5 @@ const movieRoutes = require('./routes/movieRoutes');
 app.use('/api/movies', movieRoutes);
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
